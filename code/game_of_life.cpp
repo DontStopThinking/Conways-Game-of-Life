ffc3f08da3fed657c22185cf0b946180013e0a14
Shantanu <infoshantanu02@gmail.com>
2020-04-24 11:10:10 +0530

    Convert remaining tabs to spaces, add raylib.lib, switch to using iostream inplace of cstdio

--- code/game_of_life.cpp
+++ code/game_of_life.cpp
@@ -7,21 +7,21 @@
 
 #include "raylib.h"
 
-GameOfLife::GameOfLife(size_t w, size_t h, int cellSize)
+GameOfLife::GameOfLife(size_t w, size_t h, int m_cellSize)
 {
     std::srand((unsigned)std::time(NULL));
 
     m_fieldWidth = w;
     m_fieldHeight = h;
-    runSimulation = false;
-    this->cellSize = cellSize;
+    m_runSimulation = false;
+    this->m_cellSize = m_cellSize;
 
-    frontGrid = new Cell*[m_fieldWidth / cellSize];
-    backGrid = new Cell*[m_fieldHeight / cellSize];
-    for (size_t i = 0; i < m_fieldHeight / cellSize; i++)
+    m_frontGrid = new Cell*[m_fieldWidth / m_cellSize];
+    m_backGrid = new Cell*[m_fieldHeight / m_cellSize];
+    for (size_t i = 0; i < m_fieldHeight / m_cellSize; i++)
     {
-        frontGrid[i] = new Cell[m_fieldWidth / cellSize];
-        backGrid[i] = new Cell[m_fieldWidth / cellSize];
+        m_frontGrid[i] = new Cell[m_fieldWidth / m_cellSize];
+        m_backGrid[i] = new Cell[m_fieldWidth / m_cellSize];
     }
     setGrid();
     //clearGrid();
@@ -30,74 +30,74 @@ GameOfLife::GameOfLife(size_t w, size_t h, int cellSize)
 
 GameOfLife::~GameOfLife()
 {
-    for (size_t i = 0; i < m_fieldHeight / cellSize; i++)
+    for (size_t i = 0; i < m_fieldHeight / m_cellSize; i++)
     {
-        delete[] frontGrid[i];
+        delete[] m_frontGrid[i];
     }
-    delete[] frontGrid;
+    delete[] m_frontGrid;
 }
 
 void GameOfLife::randomizeGrid()
 {
-    for (size_t i = 0; i < m_fieldWidth / cellSize; i++)
+    for (size_t i = 0; i < m_fieldWidth / m_cellSize; i++)
     {
-        for (size_t j = 0; j < m_fieldHeight / cellSize; j++)
+        for (size_t j = 0; j < m_fieldHeight / m_cellSize; j++)
         {
-            backGrid[i][j].size = cellSize;
+            m_backGrid[i][j].size = m_cellSize;
             if (std::rand() % 100 < 90)
             {
-                backGrid[i][j].value = 0;
+                m_backGrid[i][j].value = 0;
             }
             else
             {
-                backGrid[i][j].value = 1;
+                m_backGrid[i][j].value = 1;
             }
         }
     }
-    updateFrontGrid();
+    updatem_frontGrid();
 }
 
 int GameOfLife::getCell(int x, int y)
 {
-    return frontGrid[x][y].value;
+    return m_frontGrid[x][y].value;
 }
 
 void GameOfLife::setCell(int x, int y, int val)
 {
-    backGrid[x][y].value = val;
+    m_backGrid[x][y].value = val;
 }
 
 void GameOfLife::clearGrid()
 {
-    for (size_t i = 0; i < m_fieldWidth / cellSize; i++)
+    for (size_t i = 0; i < m_fieldWidth / m_cellSize; i++)
     {
-        for (size_t j = 0; j < m_fieldHeight / cellSize; j++)
+        for (size_t j = 0; j < m_fieldHeight / m_cellSize; j++)
         {
-            backGrid[i][j].value = 0;
+            m_backGrid[i][j].value = 0;
         }
     }
-    updateFrontGrid();
+    updatem_frontGrid();
 }
 
 void GameOfLife::setGrid()
 {
-    for (size_t i = 0; i < m_fieldWidth / cellSize; i++)
+    for (size_t i = 0; i < m_fieldWidth / m_cellSize; i++)
     {
-        for (size_t j = 0; j < m_fieldHeight / cellSize; j++)
+        for (size_t j = 0; j < m_fieldHeight / m_cellSize; j++)
         {
-            backGrid[i][j].value = 1;
+            m_backGrid[i][j].value = 1;
         }
     }
-    updateFrontGrid();
+    updatem_frontGrid();
 }
 
-void GameOfLife::updateFrontGrid()
+void GameOfLife::updatem_frontGrid()
 {
-    for (size_t i = 0; i < m_fieldWidth / cellSize; i++)
+    for (size_t i = 0; i < m_fieldWidth / m_cellSize; i++)
     {
-        for (size_t j = 0; j < m_fieldHeight / cellSize; j++)
+        for (size_t j = 0; j < m_fieldHeight / m_cellSize; j++)
         {
-            frontGrid[i][j] = backGrid[i][j];
+            m_frontGrid[i][j] = m_backGrid[i][j];
         }
     }
 }
@@ -105,49 +105,48 @@ void GameOfLife::updateFrontGrid()
 void GameOfLife::run()
 {
     handleInput();
-    if (runSimulation)
+    if (m_runSimulation)
     {
         updateGrid();
     }
-    //draw();
 }
 
 void GameOfLife::handleInput()
 {
     if (IsKeyPressed(KEY_SPACE))
     {
-        if (runSimulation == false)
+        if (m_runSimulation == false)
         {
             std::printf("Simulation run...\n");
-            runSimulation = true;
+            m_runSimulation = true;
             SetTargetFPS(10);
         }
         else
         {
             std::printf("Simulation paused...\n");
-            runSimulation = false;
+            m_runSimulation = false;
             SetTargetFPS(60);
         }
     }
 
-    if (!runSimulation)
+    if (!m_runSimulation)
     {
         int mouseX, mouseY;
         if (IsMouseButtonDown(MOUSE_LEFT_BUTTON))
         {
-            mouseX = GetMouseX() / cellSize;
-            mouseY = GetMouseY() / cellSize;
+            mouseX = GetMouseX() / m_cellSize;
+            mouseY = GetMouseY() / m_cellSize;
             //std::printf("mouseX: %d, mouseY: %d\n", mouseX, mouseY);
-            backGrid[mouseX][mouseY].value = 1;
-            updateFrontGrid();
+            m_backGrid[mouseX][mouseY].value = 1;
+            updatem_frontGrid();
         }
         else if (IsMouseButtonPressed(MOUSE_RIGHT_BUTTON))
         {
-            mouseX = GetMouseX() / cellSize;
-            mouseY = GetMouseY() / cellSize;
+            mouseX = GetMouseX() / m_cellSize;
+            mouseY = GetMouseY() / m_cellSize;
             //std::printf("mouseX: %d, mouseY: %d\n", mouseX, mouseY);
-            backGrid[mouseX][mouseY].value = 0;
-            updateFrontGrid();
+            m_backGrid[mouseX][mouseY].value = 0;
+            updatem_frontGrid();
         }
         else if (IsKeyPressed(KEY_C))
         {
@@ -158,13 +157,13 @@ void GameOfLife::handleInput()
 
 void GameOfLife::updateGrid()
 {
-    if (runSimulation)
+    if (m_runSimulation)
     {
-        updateFrontGrid();
+        updatem_frontGrid();
 
-        for (size_t x = 1; x < (m_fieldWidth / cellSize) - 1; x++)
+        for (size_t x = 1; x < (m_fieldWidth / m_cellSize) - 1; x++)
         {
-            for (size_t y = 1; y < (m_fieldHeight / cellSize) - 1; y++)
+            for (size_t y = 1; y < (m_fieldHeight / m_cellSize) - 1; y++)
             {
                 int east = getCell(x - 1, y);
                 int west = getCell(x + 1, y);
@@ -191,7 +190,7 @@ void GameOfLife::updateGrid()
                         setCell(x, y, 0);
                     }
                 }
-                else if (getCell(x, y) == 0)	// if current cell is dead
+                else if (getCell(x, y) == 0)    // if current cell is dead
                 {
                     if (totalNeighboursCount == 3)
                     {
@@ -205,43 +204,39 @@ void GameOfLife::updateGrid()
 
 void GameOfLife::draw()
 {
-    //BeginDrawing();
-
     int x = 0, y = 0;
-    for (size_t i = 0; i < m_fieldWidth / cellSize; i++)
+    for (size_t i = 0; i < m_fieldWidth / m_cellSize; i++)
     {
         y = 0;
-        for (size_t j = 0; j < m_fieldHeight / cellSize; j++)
+        for (size_t j = 0; j < m_fieldHeight / m_cellSize; j++)
         {
             if (getCell(i, j) == 1)
             {
-                DrawRectangle(x, y, cellSize, cellSize, liveCellColor);
-                //DrawCircle(x + cellSize / 2, y + cellSize / 2, cellSize / 2, liveCellColor);
+                DrawRectangle(x, y, m_cellSize, m_cellSize, liveCellColor);
+                //DrawCircle(x + m_cellSize / 2, y + m_cellSize / 2, m_cellSize / 2, liveCellColor);
             }
             else
             {
-                DrawRectangle(x, y, cellSize, cellSize, deadCellColor);
-                //DrawCircle(x + cellSize / 2, y + cellSize / 2, cellSize / 2, deadCellColor);
+                DrawRectangle(x, y, m_cellSize, m_cellSize, deadCellColor);
+                //DrawCircle(x + m_cellSize / 2, y + m_cellSize / 2, m_cellSize / 2, deadCellColor);
             }
-            y += cellSize;
+            y += m_cellSize;
         }
-        x += cellSize;
+        x += m_cellSize;
     }
 
-    // Draw grid
+    // Draws grid lines
     for (size_t i = 0; i < m_fieldWidth + 1; i++)
     {
-        DrawLine(i * cellSize, 0, i * cellSize, m_fieldHeight + 1, gridOutlineColor);
+        DrawLine(i * m_cellSize, 0, i * m_cellSize, m_fieldHeight + 1, gridOutlineColor);
     }
     for (size_t i = 0; i < m_fieldHeight + 1; i++)
     {
-        DrawLine(0, i * cellSize, m_fieldWidth + 1, i * cellSize, gridOutlineColor);
+        DrawLine(0, i * m_cellSize, m_fieldWidth + 1, i * m_cellSize, gridOutlineColor);
     }
-
-    //EndDrawing();
 }
 
 bool GameOfLife::isSimulationInProgress()
 {
-    return runSimulation;
+    return m_runSimulation;
 }
--- code/game_of_life.h
+++ code/game_of_life.h
@@ -7,25 +7,25 @@ private:
 
     size_t m_fieldWidth;
     size_t m_fieldHeight;
-    bool runSimulation = false;
-    int cellSize;
+    bool m_runSimulation = false;
+    int m_cellSize;
 
-    Cell **backGrid;	// used as a backbuffer to update all cells at once before drawing
-    Cell **frontGrid;	// actually used to draw the cells
+    Cell **m_backGrid;    // used as a backbuffer to update all cells at once before drawing
+    Cell **m_frontGrid;   // actually used to draw the cells
 
     // private methods
-    void updateGrid();						// updates the back grid cells as per the rules of Conway's Game of Life
+    void updateGrid();                   // updates the back grid cells as per the rules of Conway's Game of Life
     void randomizeGrid();
-    int getCell(int x, int y);				// returns the front grid cell value at (x, y)
-    void setCell(int x, int y, int val);	// sets the back grid cell at (x, y) to val
+    int getCell(int x, int y);	         // returns the front grid cell value at (x, y)
+    void setCell(int x, int y, int val); // sets the back grid cell at (x, y) to val
     void handleInput();
-    void updateFrontGrid();					// syncs the front grid with the back grid
-    void clearGrid();						// sets all cells value to 0
-    void setGrid();							// sets all cells to 1
+    void updatem_frontGrid();	         // syncs the front grid with the back grid
+    void clearGrid();                    // sets all cells value to 0
+    void setGrid();	                     // sets all cells to 1
 
 public:
 
-    GameOfLife(size_t w, size_t h, int cellSize);
+    GameOfLife(size_t w, size_t h, int m_cellSize);
     ~GameOfLife();
 
     void run();
